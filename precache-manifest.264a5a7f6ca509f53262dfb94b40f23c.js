71f899d69cfbc4e7637e57b309658f08a7b5388a
NitinPokhriyal30 <32007748+NitinPokhriyal30@users.noreply.github.com>
2018-11-17 00:50:37 +0530

    Updates

---
+++ precache-manifest.264a5a7f6ca509f53262dfb94b40f23c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "78d589a45e62f8f1c2da",
+    "url": "/poolmyride-app/static/js/runtime~main.78d589a4.js"
+  },
+  {
+    "revision": "b4534418b54bbadd366e",
+    "url": "/poolmyride-app/static/js/main.b4534418.chunk.js"
+  },
+  {
+    "revision": "c66725e4872d08bd9eed",
+    "url": "/poolmyride-app/static/js/1.c66725e4.chunk.js"
+  },
+  {
+    "revision": "b4534418b54bbadd366e",
+    "url": "/poolmyride-app/static/css/main.9dd27658.chunk.css"
+  },
+  {
+    "revision": "c66725e4872d08bd9eed",
+    "url": "/poolmyride-app/static/css/1.36be82bb.chunk.css"
+  },
+  {
+    "revision": "d22ca7573efcfd9a65629b830c71a0b0",
+    "url": "/poolmyride-app/index.html"
+  }
+];
